9c8ecc3c1b3c36e2ec36446e49b06408be218955
Raj Gondalia <rmgondalia7@gmail.com>
2023-03-07 12:56:28 +0000

    initial commit

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "var.region"
+  access_key = "yOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
+}
+
+resource "aws_instance" "a1" {
+  ami = "ami-04706e771f950937f"
+  instance_type = "t2.large"
+  count = 5 # ignore changes
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
+}
